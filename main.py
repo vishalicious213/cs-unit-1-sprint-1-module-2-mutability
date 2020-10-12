4b928efaad062328cbc2afc281e25743a9ac5980
Vish Singh <vishalicious213@gmail.com>
2020-10-12 04:10:44 +0000

    completed example 2

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ Example One
 my_list1 = [1, 2, 3, 4, 5, 6]
 my_list2 = my_list1
 # How would you verify that my_list1 and my_list2 have the same identity?
-print(id(my_list1))
-print(id(my_list2))
+print("list1 ", id(my_list1))
+print("list2 ", id(my_list2))
 
 my_list1.append(7)
 # Check if my_list1 and my_list2 still have the same identity.
 # If they do, why is that?
-print(id(my_list1))
-print(id(my_list2))
+print("list1 v2 ", id(my_list1))
+print("list2 v2 ", id(my_list2))
 # because my_list2 is set to equal (=) my_list1. They point the same object in memory.
 
 
@@ -21,17 +21,21 @@ Example Two
 my_text1 = "Lambda School"
 my_text2 = my_text1
 # How would you verify that my_text1 and my_text2 have the same identity?
-print("your code here")
+print("text1 ", id(my_text1))
+print("text2 ", id(my_text2))
 
 my_text1 += " is awesome!"
 # Check if my_text1 and my_text2 still have the same identity?
-print("your code here")
+print("text1 v2 ", id(my_text1))
+print("text2 v2 ", id(my_text2))
 # If they do not, why is that?
+# text2 points to the original text1. text1 changed and has a new address.
 
 # Now check if my_text1 and my_text2 have the same value?
-print("your code here")
+print(my_text1)
+print(my_text2)
 # Do they? Explain why or why not.
-
+# No, because text1 was appended to and altered. text2 remained the same.
 
 
 """
